436f0bebe8fcba0e8ee2bc573e4050ab67b6277d
Eduardo Ramos <eduardoramos15975@gmail.com>
2024-08-01 18:04:20 -0300

    Método criar Ninja implementado

--- src/main/java/com/eduardo/ApiNaruto/controller/NinjasController.java
+++ src/main/java/com/eduardo/ApiNaruto/controller/NinjasController.java
@@ -1,7 +1,12 @@
 package com.eduardo.ApiNaruto.controller;
 
+import com.eduardo.ApiNaruto.model.Ninjas;
 import com.eduardo.ApiNaruto.service.NinjasService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +16,11 @@ public class NinjasController {
 
     @Autowired
     private NinjasService service;
+
+    @PostMapping("/add")
+    public ResponseEntity<Ninjas> addNinja(@RequestBody Ninjas ninja) {
+        Ninjas newNinja = service.createNinja(ninja);
+        return new ResponseEntity<>(newNinja, HttpStatus.CREATED);
+    }
 
 }
--- src/main/java/com/eduardo/ApiNaruto/service/NinjasService.java
+++ src/main/java/com/eduardo/ApiNaruto/service/NinjasService.java
@@ -1,5 +1,6 @@
 package com.eduardo.ApiNaruto.service;
 
+import com.eduardo.ApiNaruto.model.Ninjas;
 import com.eduardo.ApiNaruto.repository.NinjasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,10 @@ public class NinjasService {
 
     @Autowired
     private NinjasRepository repository;
+
+    // criar um novo ninja
+    public Ninjas createNinja(Ninjas ninja) {
+        return repository.save(ninja);
+    }
 
 }
